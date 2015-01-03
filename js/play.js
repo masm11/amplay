e17dcdad24667f8caf81ef0e32215102279c356c
Yuuki Harano <masm@masm11.ddo.jp>
2015-01-03 19:55:34 +0900

    set_msg() 作成、対応。

--- js/play.js
+++ js/play.js
@@ -1,36 +1,39 @@
 var files = [];
 var curidx = 0;
 
+function set_msg(str) {
+    document.getElementById('msg').firstChild.nodeValue = str;
+}
+
 var storage = navigator.getDeviceStorage('music');
 var cursor = storage.enumerate();
 cursor.onsuccess = function() {
     if (this.result) {
 	var file = this.result;
-	document.getElementById('msg').firstChild.nodeValue = file.name;
-//	files.push(file.name);
+	set_msg(file.name);
 	files.push(file);
 	this.continue();
     } else {
-	document.getElementById('msg').firstChild.nodeValue = 'done.';
+	set_msg('done.');
 //	play_next();
-document.getElementById('msg').firstChild.nodeValue = 'd1.';
+	set_msg('d1.');
 
 
 var audio_context = new AudioContext();
-document.getElementById('msg').firstChild.nodeValue = 'd2.';
+set_msg('d2.');
 var request = new XMLHttpRequest({mozSystem: true});
 // var request = new XMLHttpRequest();
-document.getElementById('msg').firstChild.nodeValue = 'k1.';
-document.getElementById('msg').firstChild.nodeValue = 'k11.' + URL.createObjectURL(files[0]);
+set_msg('k1.');
+set_msg('k11.' + URL.createObjectURL(files[0]));
 // request.open('GET', URL.createObjectURL(files[0]), true);
 
 // var audio = new Audio(URL.createObjectURL(files[0]));
 // audio.play();
 
-document.getElementById('msg').firstChild.nodeValue = 'a: ' + document.getElementById('audio');
+set_msg('a: ' + document.getElementById('audio'));
 document.getElementById('audio').src = window.URL.createObjectURL(files[0]);
 // document.getElementById('audio').src = 'sound/doll_st_01.ogg';
-document.getElementById('msg').firstChild.nodeValue = 'b: ' + document.getElementById('audio').src;
+set_msg('b: ' + document.getElementById('audio').src);
 
 /*
 // request.open('GET', '/sdcard/Music/rusur/hm04.wav', true);
@@ -42,46 +45,46 @@ document.getElementById('msg').firstChild.nodeValue = 'b: ' + document.getElemen
 // request.open('GET', '/rusur/haji.ogg', true);
 // request.open('GET', 'sound/doll_st_01.ogg', true);
 request.responseType = 'arraybuffer';
-document.getElementById('msg').firstChild.nodeValue = 'k2.';
+set_msg('k2.');
 request.onload = function() {
-    document.getElementById('msg').firstChild.nodeValue = 'k6.';
+    set_msg('k6.');
     audio_context.decodeAudioData(request.response,
 				  function(buffer) {
 				      //
-				      document.getElementById('msg').firstChild.nodeValue = 'k8.';
+				      set_msg('k8.');
 				      var source = audio_context.createBufferSource();
-				      document.getElementById('msg').firstChild.nodeValue = 'k9 ' + source;
+				      set_msg('k9 ' + source);
 				      source.buffer = buffer;
 				      source.connect(audio_context.destination);
 				      source.start(0);
 				      //
 				  },
 				  function(e) {
-				      document.getElementById('msg').firstChild.nodeValue = 'decode error.';
+				      set_msg('decode error.');
 				  });
-    document.getElementById('msg').firstChild.nodeValue = 'k7.';
+    set_msg('k7.');
 }
-document.getElementById('msg').firstChild.nodeValue = 'k3.';
+set_msg('k3.');
 request.onerror = function() {
-    document.getElementById('msg').firstChild.nodeValue = 'error.';
+    set_msg('error.');
 }
-document.getElementById('msg').firstChild.nodeValue = 'k4.';
+set_msg('k4.');
 request.send();
-document.getElementById('msg').firstChild.nodeValue = 'k5.';
+set_msg('k5.');
 */
 
 /*
 var audio_context = new AudioContext();
 var request = storage.get('/sdcard/Music/rusur/hm04.wav');
 // var request = storage.get('/sdcard///Music/rusur/hm04.wav');
-document.getElementById('msg').firstChild.nodeValue = 'kita1 ' + request;
+set_msg('kita1 ' + request);
 request.onsuccess = function() {
-    document.getElementById('msg').firstChild.nodeValue = 'kita2';
+    set_msg('kita2');
     var file = this.result;
-    document.getElementById('msg').firstChild.nodeValue = 'kita3';
+    set_msg('kita3');
     var data = file.getAsBinary();
-    document.getElementById('msg').firstChild.nodeValue = 'kita4';
-    document.getElementById('msg').firstChild.nodeValue = '' + data;
+    set_msg('kita4');
+    set_msg('' + data);
 
     var source = audio_context.createBufferSource();
     source.buffer = data;
@@ -89,9 +92,9 @@ request.onsuccess = function() {
     source.start(0);
 };
 request.onerror = function() {
-    document.getElementById('msg').firstChild.nodeValue = '' + this.error.name;
+    set_msg('' + this.error.name);
 };
-document.getElementById('msg').firstChild.nodeValue = 'kita5';
+set_msg('kita5');
 */
 
     }
@@ -103,7 +106,7 @@ cursor.onerror = function() {
 function play_next() {
     if (++curidx >= files.length)
 	curidx = 0;
-    document.getElementById('msg').firstChild.nodeValue = '' + files[curidx];
+    set_msg('' + files[curidx]);
     audio = new Audio(files[curidx]);
     audio.play();
 //    audio.addEventListener('ended', play_next);
@@ -115,8 +118,8 @@ function play_next() {
 }
 
 function play_this(e) {
-    document.getElementById('msg').firstChild.nodeValue = 'play_this';
-    document.getElementById('msg').firstChild.nodeValue = '' + document.getElementById('file').value;
+    set_msg('play_this');
+    set_msg('' + document.getElementById('file').value);
     
     e.preventDefault();
 }
@@ -142,9 +145,9 @@ window.onload = function() {
 // play_next();
 
 window.onload = function() {
-    document.getElementById('msg').firstChild.nodeValue = 'onload1';
+    set_msg('onload1');
     pt = document.getElementById('play_this')
-    document.getElementById('msg').firstChild.nodeValue = 'onload2';
+    set_msg('onload2');
     pt.addEventListener('click', play_this, false);
-    document.getElementById('msg').firstChild.nodeValue = 'onload3';
+    set_msg('onload3');
 };
